d96810c490bdb8b93f1b2f5f98a1d9b8195354ff
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-01-12 19:16:50 -0500

    CDRIVER-959 build with MinGW-W64 in Evergreen

--- .evergreen/compile.sh
+++ .evergreen/compile.sh
@@ -2,6 +2,12 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
+case "$CC" in
+   mingw*)
+      cmd.exe /c .evergreen\\compile.bat
+      exit 0
+   ;;
+esac
 
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
