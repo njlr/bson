d943747226cb3ea1a3f2d4cbd78652cb69f2583e
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-01-16 19:56:00 -0500

    response to critique

--- .evergreen/compile.sh
+++ .evergreen/compile.sh
@@ -2,13 +2,6 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
-case "$CC" in
-   mingw*)
-      cmd.exe /c .evergreen\\compile.bat
-      exit 0
-   ;;
-esac
-
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
 case "$OS" in
